4bcbcb1dede7ee105e3b5278cf3ec8540ddcd44f
2272799702lxd <m15871692767@163.com>
2018-10-31 20:30:15 +0800

    modify

---
+++ src/main/java/com/almybaties/parsing/AlPropertyParser.java
@@ -0,0 +1,17 @@
+package com.almybaties.parsing;
+
+/**
+ * Property Parse : handle properties with variables
+ * @author  adonai
+ */
+public class AlPropertyParser {
+
+    /**
+     * class full qualify name
+     */
+    private static final String KEY_PREFIX = "com.almybaties.parsing.AlPropertyParser.";
+
+    
+
+
+}
